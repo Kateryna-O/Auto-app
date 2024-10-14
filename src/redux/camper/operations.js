c5c4a2b8840b9277599e720f9f09f68ddd54f5c9
Kateryna-O <osadcukkata88@gmail.com>
2024-10-14 03:16:13 +0300

    Update operations.js

--- src/redux/camper/operations.js
+++ src/redux/camper/operations.js
@@ -10,7 +10,6 @@ export const fetchCampers = createAsyncThunk(
       const response = await axios.get(
         `${baseURL}?page=${page}&limit=${itemsPerPage}`
       );
-      console.log(response.data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
